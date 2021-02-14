c4483014a461d3208977e960b8b7f69a5c166e3a
aarushi2002 <75518514+aarushi2002@users.noreply.github.com>
2021-02-14 16:58:40 +0530

    Gasoline and Hydro Calculator added

---
+++ Volte Gasoline  and Hydro Estimator.py
@@ -0,0 +1,5 @@
+fam=int(input("Enter number of family members in your house:- "))
+times=int(input("Enter number of times your family take proper meal:- "))
+
+print('Approx Cylinder consumption in a month',round(10/(48*3)*fam*times))
+print("Average water family uses",90*fam*30,"gallons per month")
