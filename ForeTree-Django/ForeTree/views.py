64d65496fbe631482a85c559e379963d9e862858
AADI JAIN <jainaadi7102@gmail.com>
2022-01-15 17:43:23 +0530

    Add files via upload

---
+++ ForeTree-Django/ForeTree/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+    return render(request,'home.html')
+def about(request):
+    return render(request,'About.html')
+def mission(request):
+    return render(request,'mission.html')
+def contact(request):
+    return render(request,'contact.html')
+
